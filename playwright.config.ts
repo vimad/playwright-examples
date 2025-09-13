0f62f25ae25970fcc046648b13b997c033f3deed
Vinod Madubashana <vinod@wealthos.cloud>
2025-09-13 21:38:20 +0530

    add webserver to playwright.config.ts and some refactor

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
     testDir: './tests',
-    reporter: 'html'
+    reporter: 'html',
+
+    webServer: {
+        command: 'npm run start',
+        url: 'http://localhost:3000',
+    }
 });
